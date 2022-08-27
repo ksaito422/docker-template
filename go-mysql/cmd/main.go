a8fd575ab1d9d8b4b65ab090c59ba886723135f1
saitooooooo <kitsit.39@gmail.com>
2022-08-27 18:35:54 +0900

    Add: go mysql connection

--- go-mysql/cmd/main.go
+++ go-mysql/cmd/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"go-mysql/pkg"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func handler(w http.ResponseWriter, _ *http.Request) {
@@ -10,6 +15,14 @@ func handler(w http.ResponseWriter, _ *http.Request) {
 }
 
 func main() {
+	dbconf := "admin:secret@tcp(db-backend-go:3306)/default?charset=utf8mb4"
+	db := myapp.OpenDB("mysql", dbconf)
+	defer myapp.CloseDB(db)
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("db.Ping failed: ", err)
+	}
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8000", nil)
 }

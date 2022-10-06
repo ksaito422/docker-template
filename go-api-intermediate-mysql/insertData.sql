3dc66b0ca72fa9cba4d53d74be053c40cffbc15d
kt-saito <k.saito@impl.co.jp>
2022-10-06 09:10:27 +0900

    add: 疎通確認

---
+++ go-api-intermediate-mysql/insertData.sql
@@ -0,0 +1,10 @@
+insert into articles (title, contents, username, nice, created_at) values
+	('firstPost', 'This is my first blog', 'keita', 2, now());
+insert into articles (title, contents, username, nice) values
+	('2nd', 'Second blog post', 'keita', 4);
+
+insert into comments (article_id, message, created_at) values
+	(1, '1st comment yeah', now());
+insert into comments (article_id, message) values
+	(1, 'welcome');
+
